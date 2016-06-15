b76d04444929e3537dbc23df50a251ade8baaaae
Alex Robertson <lex1989@gmail.com>
2016-06-15 17:05:59 +0100

    implementing karma tests

--- test/unit/FoodProductController.spec.js
+++ test/unit/FoodProductController.spec.js
@@ -1,19 +1,37 @@
 describe('FoodProductController', function() {
-  var controller;
+
+  module(function($provide) {
+    $provide.service('FoodProductService', function() {
+      this.getProductInfo = jasmine.createSpy('getProductInfo').andCallFake(function(barcode) {
+        return 'food';
+      });
+    });
+  });
+
+  var controller, service;
 
   beforeEach(module('happyBellyApp'));
 
-  beforeEach(inject(function($controller) {
+  beforeEach(inject(function($controller, FoodProductService) {
     controller = $controller('FoodProductController');
+    service = FoodProductService;
   }));
+
+
 
   it('is initialized with no food product', function() {
     expect(controller.foodProduct).not.toBeDefined();
-  })
+  });
 
-  // describe('#getProductInfo', function() {
-  //   it('sets the foodProduct attribute' function() {
-
-  //   });
-  // });
+  describe('#getProductInfo', function() {
+    spyOn(service, 'getProductInfo');
+    it('sets the foodProduct attribute', function() {
+      controller.getProductInfo(1234);
+      expect(controller.foodProduct).toEqual('food');
+    });
+    it('calls the service', function() {
+      controller.getProductInfo(1234);
+      expect(service.getProductInfo).toHaveBeenCalled();
+    });
+  });
 });
