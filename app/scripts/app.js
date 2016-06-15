8677b04a02235d7f3cc58d2ae82cb5282e80d02b
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-15 20:16:10 +0100

    tweaked view, added both routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('happyBellyApp', ['ionic', 'ng-token-auth'])
 
 
@@ -26,10 +21,14 @@ angular.module('happyBellyApp', ['ionic', 'ng-token-auth'])
 
 .config(function($stateProvider, $urlRouterProvider, $authProvider) {
   $stateProvider
-    .state('search', {
-      url: '/',
+    .state('sign_up', {
+      url: '/sign_up',
       templateUrl: 'views/sign_up.html'
-    });
+    })
+    .state('search', {
+      url: '/search',
+      templateUrl: 'views/search.html'
+    })
 
   $urlRouterProvider.otherwise('/');
 
