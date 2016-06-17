898da7e6970704583560db77d8ec097f5fbee7a8
Alex Robertson <lex1989@gmail.com>
2016-06-17 12:28:43 +0100

    Trying to hide login buttons

--- app/scripts/controllers/UserController.js
+++ app/scripts/controllers/UserController.js
@@ -1,13 +1,30 @@
 angular.module('happyBellyApp')
   .controller('UserController', function($scope, $auth, $state){
+
+    var self = this;
+
+
     $scope.handleRegBtnClick = function() {
       $auth.submitRegistration($scope.registrationForm)
         .then(function(resp) {
+          self.currentUser = true;
+          console.log(self.currentUser);
+          console.log('Should be true');
          $state.go('search');
         })
         .catch(function(resp) {
         console.log(resp);
+        self.currentUser = false;
         });
     };
 
+    self.userCheck = function() {
+      console.log(self.currentUser);
+    };
+
+    // function findUser() {
+    //   $auth.validateUser().then(function(resp) {
+    //     self.currentUser = resp.signedIn;
+    //   });
+    // }
   });
