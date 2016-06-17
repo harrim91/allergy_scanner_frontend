1ec9aba92951f35e161c6c63d8ca83cf6f27552c
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-17 14:45:04 +0100

    stopped using scope

--- app/scripts/controllers/FoodProductController.js
+++ app/scripts/controllers/FoodProductController.js
@@ -1,10 +1,11 @@
 angular.module('happyBellyApp')
-  .controller('FoodProductController', ['FoodProductService', '$state', function(FoodProductService, $state, $scope, $cordovaBarcodeScanner){
+  .controller('FoodProductController', ['FoodProductService', '$state', function(FoodProductService, $state, $cordovaBarcodeScanner){
 
     var self = this;
     var API_URL = 'http://world.openfoodfacts.org/api/v0/product/';
 
     self.getProductInfo = getProductInfo;
+    self.scanBarcode = scanBarcode;
 
     function getProductInfo(barcode) {
       FoodProductService.getProductInfo(API_URL, barcode).then(function(response) {
@@ -13,7 +14,7 @@ angular.module('happyBellyApp')
       _redirectToProductInfo();
     }
 
-    $scope.scanBarcode = function() {
+    function scanBarcode() {
       $cordovaBarcodeScanner.scan().then(function(imageData){
         self.getProductInfo(imageData.text);
       }, function(error){
