2ae416012bb2b9ce619427309b7c20008a19f966
Alex Robertson <lex1989@gmail.com>
2016-06-18 12:48:53 +0100

    Fixed barcode scanner

--- app/scripts/controllers/FoodProductController.js
+++ app/scripts/controllers/FoodProductController.js
@@ -1,11 +1,11 @@
 angular.module('happyBellyApp')
-  .controller('FoodProductController', ['FoodProductService', '$state', function(FoodProductService, $state, $cordovaBarcodeScanner){
+  .controller('FoodProductController', function($scope, FoodProductService, $state, $cordovaBarcodeScanner){
 
     var self = this;
     var API_URL = 'http://world.openfoodfacts.org/api/v0/product/';
 
     self.getProductInfo = getProductInfo;
-    self.scanBarcode = scanBarcode;
+    // self.scanBarcode = scanBarcode;
 
     function getProductInfo(barcode) {
       FoodProductService.getProductInfo(API_URL, barcode).then(function(response) {
@@ -14,16 +14,25 @@ angular.module('happyBellyApp')
       _redirectToProductInfo();
     }
 
-    function scanBarcode() {
+    // self.scanBarcode = function() {
+    //   console.log('Scan in progress');
+    //   $cordovaBarcodeScanner.scan().then(function(imageData){
+    //     self.getProductInfo(imageData.text);
+    //   }, function(error){
+    //     console.log('an error has occured ' + error);
+    //   });
+    // };
+
+    $scope.scanBarcode = function(){
       $cordovaBarcodeScanner.scan().then(function(imageData){
-        self.getProductInfo(imageData.text);
-      }, function(error){
-        console.log('an error has occured ' + error);
-      });
-    }
+      self.getProductInfo(imageData.text);
+    }, function(error){
+     console.log('an error has occured ' + error);
+   });
+ };
 
     function _redirectToProductInfo() {
       $state.go('product-info');
     }
 
-  }]);
+  });
