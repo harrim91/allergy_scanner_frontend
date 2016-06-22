127d10014e6101f4c119c3cfe8357b975fb2fedb
sitypop <sityemilyshah@gmail.com>
2016-06-22 11:42:04 +0100

    Adds back in, routes don't seem to work without it...

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -79,4 +79,11 @@ angular.module('happyBellyApp', ['ionic', 'ng-token-auth', 'ngCordova'])
 
   $urlRouterProvider.otherwise('/');
 
+  $authProvider.configure({
+    apiUrl: 'http://happy-belly-api.herokuapp.com/',
+    authProviderPaths: {
+      facebook: '/auth/facebook'
+    }
+  });
+
 });
