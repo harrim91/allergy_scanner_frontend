7b16853e6bd9137225532cc871ed150d1c064a73
Alex Robertson <lex1989@gmail.com>
2016-06-22 15:45:10 +0100

    Implemented ingredients comparison

--- app/scripts/controllers/FoodProductController.js
+++ app/scripts/controllers/FoodProductController.js
@@ -2,8 +2,23 @@ angular.module('happyBellyApp')
   .controller('FoodProductController', function($scope, FoodProductService, UserProfileService, $state, $cordovaBarcodeScanner){
 
     var self = this;
+    self.compareIngredients = compareIngredients
 
     self.foodProductInfo = FoodProductService.foodProductInfo;
+    // console.log(UserProfileService.userIngredients);
+    // console.log(self.foodProductInfo.ingredients);
 
-    console.log(UserProfileService.userIngredients);
+
+    function compareIngredients(product, user) {
+      var result = [];
+      for(var i in product) {
+          if(user.indexOf( product[i] ) > -1){
+              result.push( product[i] );
+          }
+      }
+      return result.length === 0;
+    }
+
+    console.log(self.compareIngredients(self.foodProductInfo.ingredients, UserProfileService.userIngredients));
+
   });
