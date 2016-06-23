0cee4f07e6254267e2c89b37e24e0769f631762c
sitypop <sityemilyshah@gmail.com>
2016-06-23 20:10:33 +0100

    Almost done apart from product info page

--- app/scripts/controllers/FoodProductController.js
+++ app/scripts/controllers/FoodProductController.js
@@ -6,13 +6,13 @@ angular.module('happyBellyApp')
     self.foodProductInfo = FoodProductService.foodProductInfo;
 
     function compareIngredients(user, product) {
-      var result = [];
+      self.iCantEat = [];
       for(var ingredient in user) {
           if(product.indexOf(user[ingredient]) > -1){
-              result.push(user[ingredient]);
+              self.iCantEat.push(user[ingredient]);
           }
       }
-      return result.length === 0;
+      return self.iCantEat.length === 0;
     }
 
     self.canIEatThis = compareIngredients(UserProfileService.userIngredients, self.foodProductInfo.ingredients);
